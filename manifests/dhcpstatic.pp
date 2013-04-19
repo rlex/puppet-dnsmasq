f1ad919361e5d646d5a25194c039c1d03cc2d652
Lex Rivera <me@lex.io>
2013-04-19 17:16:56 +0400

    [staticdhcp] -> ::dhcpstatic

--- manifests/dhcpstatic.pp
+++ manifests/dhcpstatic.pp
@@ -1,5 +1,5 @@
 # Create an dnsmasq stub zone for caching upstream name resolvers.
-define dnsmasq::staticdhcp (
+define dnsmasq::dhcpstatic (
   $mac,
   $macip,
 ) {
@@ -7,7 +7,7 @@ define dnsmasq::staticdhcp (
 
   $dnsmasq_conffile = $dnsmasq::params::dnsmasq_conffile
 
-  concat::fragment { "dnsmasq-staticdhcp-$staticip":
+  concat::fragment { "dnsmasq-staticdhcp-$name":
     order   => '02',
     target  => "${dnsmasq_conffile}",
     content => template("dnsmasq/staticdhcp.erb"),

b3ed7a5ca1e6969b06458208d8ca1979fbc8681d
Lex Rivera <me@lex.io>
2013-04-19 17:17:49 +0400

    [dhcpoption] added for adding dhcp options

---
+++ manifests/dhcpoption.pp
@@ -0,0 +1,15 @@
+# Create an dnsmasq dhcp option.
+define dnsmasq::dhcpoption (
+  $content,
+) {
+  include dnsmasq::params
+
+  $dnsmasq_conffile = $dnsmasq::params::dnsmasq_conffile
+
+  concat::fragment { "dnsmasq-dhcpoption-$name":
+    order   => '02',
+    target  => "${dnsmasq_conffile}",
+    content => template("dnsmasq/dhcpoption.erb"),
+  }
+
+}

8fec04ebc7ce19b9baf20688eb89a8f1f09a2d66
Lex Rivera <me@lex.io>
2013-04-23 15:40:29 +0400

    [dhcpstatic] even more style-fixes

--- manifests/dhcpstatic.pp
+++ manifests/dhcpstatic.pp
@@ -7,10 +7,10 @@ define dnsmasq::dhcpstatic (
 
   $dnsmasq_conffile = $dnsmasq::params::dnsmasq_conffile
 
-  concat::fragment { "dnsmasq-staticdhcp-$name":
+  concat::fragment { "dnsmasq-staticdhcp-${name}":
     order   => '02',
-    target  => "${dnsmasq_conffile}",
-    content => template("dnsmasq/staticdhcp.erb"),
+    target  => $dnsmasq_conffile,
+    content => template('dnsmasq/staticdhcp.erb'),
   }
 
 }

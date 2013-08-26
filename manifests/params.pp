fe9a9bb920ed990c948eb1224bfee5f782eda576
Lex Rivera <me@lex.io>
2013-08-26 17:04:40 +0400

    [core] add support for dnsmasq.d config folder

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,24 +4,28 @@ class dnsmasq::params {
     'ubuntu', 'debian': {
       $dnsmasq_conffile = '/etc/dnsmasq.conf'
       $dnsmasq_logdir = '/var/log'
+      $dnsmasq_confdir = '/etc/dnsmasq.d'
       $dnsmasq_service = 'dnsmasq'
       $dnsmasq_package = 'dnsmasq'
     }
     'redhat', 'centos', 'scientific': {
       $dnsmasq_conffile = '/etc/dnsmasq.conf'
       $dnsmasq_logdir = '/var/log'
+      $dnsmasq_confddir = '/etc/dnsmasq.d'
       $dnsmasq_service = 'dnsmasq'
       $dnsmasq_package = 'dnsmasq'
     }
     'darwin': {
       $dnsmasq_conffile = '/opt/local/etc/dnsmasq.conf'
       $dnsmasq_logdir  = '/opt/local/var/log/dnsmasq'
+      $dnsmasq_confdir = '/opt/local/etc/dnsmasq.d'
       $dnsmasq_service = 'org.macports.dnsmasq'
       $dnsmasq_package = 'dnsmasq'
     }
     'freebsd': {
       $dnsmasq_conffile = '/usr/local/etc/dnsmasq.conf'
       $dnsmasq_logdir  = '/var/log/dnsmasq'
+      $dnsmasq_confdir = '/usr/local/etc/dnsmasq.d'
       $dnsmasq_service = 'dnsmasq'
       $dnsmasq_package = 'dns/dnsmasq'
     }

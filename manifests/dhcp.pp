d9104e8a264450b63e0f52604c4fe6fdecb27cdd
Lex Rivera <me@lex.io>
2013-09-20 15:49:35 +0400

    [dhcp] unique names for each concat fragments

--- manifests/dhcp.pp
+++ manifests/dhcp.pp
@@ -11,7 +11,7 @@ define dnsmasq::dhcp (
 
   $dnsmasq_conffile = $dnsmasq::params::dnsmasq_conffile
 
-  concat::fragment { 'dnsmasq-dhcprange':
+  concat::fragment { "dnsmasq-dhcprange-${name}":
     order   => '01',
     target  => $dnsmasq_conffile,
     content => template('dnsmasq/dhcp.erb'),

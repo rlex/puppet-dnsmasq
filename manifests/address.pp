7827b7034b9fb3c0deb55764ac17352d6f61ce91
Lex Rivera <me@lex.io>
2014-11-22 04:31:52 +0300

    [address] ordered order. Better looking

--- manifests/address.pp
+++ manifests/address.pp
@@ -7,7 +7,7 @@ define dnsmasq::address (
   $dnsmasq_conffile = $dnsmasq::params::dnsmasq_conffile
 
   concat::fragment { "dnsmasq-staticdns-${name}":
-    order   => '06',
+    order   => "06_${name}",
     target  => $dnsmasq_conffile,
     content => template('dnsmasq/address.erb'),
   }

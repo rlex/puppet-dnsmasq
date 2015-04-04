2fff65405002397a3517879881e8e7c465104d70
Lex Rivera <me@lex.io>
2015-04-04 23:31:40 +0300

    [hostrecord] add ipv6 support

--- manifests/hostrecord.pp
+++ manifests/hostrecord.pp
@@ -1,10 +1,16 @@
 # Create a dnsmasq A,AAAA and PTR record (--host-record).
 define dnsmasq::hostrecord (
   $ip,
+  $ipv6 = undef,
 ) {
   if !is_ip_address($ip) { fail("Expect IP address for ip, got ${ip}") }
 
   include dnsmasq
+
+  $ipv6_real = $ipv6 ? {
+    undef   => '',
+    default => ",${ipv6}",
+  }
 
   concat::fragment { "dnsmasq-hostrecord-${name}":
     order   => '06',

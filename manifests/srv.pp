07e5ad1d3fb0ef8da66ad00022c38af93512df7f
Ansgar Jazdzewski <ajaz@spreadshirt.net>
2016-04-07 22:44:55 +0200

    allow multible SRV.Records with the same Name (for DNS-RoundRobbin)

--- manifests/srv.pp
+++ manifests/srv.pp
@@ -1,15 +1,27 @@
 # Create an dnsmasq srv record (--srv-host).
 define dnsmasq::srv (
+  $record = undef,
   $hostname,
   $port,
   $priority = undef,
+  $weigth = undef,
 ) {
   validate_re($port,'^[0-9]+$')
   if undef != $priority { validate_re($port,'^[0-9]+$') }
 
+  $record_real = $record ? {
+    undef   => "${name}",
+    default => "${record}",
+  }
+
   $priority_real = $priority ? {
-    undef   => '',
-    default => ",${priority}",
+    undef   => ',0',
+    default => "${priority}",
+  }
+
+  $weight_real = $weight ? {
+    undef   => ',0',
+    default => "${weigth}",
   }
 
   include dnsmasq

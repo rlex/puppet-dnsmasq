bb8f24cce593de99ad076107033c0dc1df0a3985
flepoutre <84913246+flepoutre@users.noreply.github.com>
2023-12-05 10:53:32 +0100

    fix type

--- manifests/dnsserver.pp
+++ manifests/dnsserver.pp
@@ -9,8 +9,8 @@
 #
 define dnsmasq::dnsserver (
   String $ip,
-  Optional[String] $domain = undef,
-  Optional[String] $port   = undef,
+  Optional[String] $domain                 = undef,
+  Optional[Variant[String, Integer]] $port = undef,
 ) {
   $domain_real = $domain ? {
     undef   => '',
